a0d383577022ad364670126205f17a5f2acf4516
鮫島広朗 <hanasaita087@gmail.com>
2021-01-03 13:26:34 +0900

    ()を使い計算の優先度を変更する

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ echo 10 - 3 . PHP_EOL;  //7
 echo 10 * 3 . PHP_EOL;  //30
 echo 10 / 3 . PHP_EOL;  //3.3333333
 
-
 echo 10 % 3 . PHP_EOL;  //1
 echo 10 ** 3 . PHP_EOL;  //1000
 
+//優先度の高い計算式
+
+echo 2 + 10 * 3 .PHP_EOL; //32
+echo (2 + 10) * 3 .PHP_EOL; //36
