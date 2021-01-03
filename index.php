c380346e2d281ef1275f676f52e8e8fda09e59ae
鮫島広朗 <hanasaita087@gmail.com>
2021-01-03 18:36:02 +0900

    型の変更(19,20行)

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//変数の型を調べる
+//型を変更する
 
 $a =  'hello';
 $b =  10;
@@ -8,13 +8,20 @@ $d =  null;
 $e = true;
 
 //変数の型を調べる
+
+// var_dump($a);
+// var_dump($b);
+// var_dump($c);
+// var_dump($d);
+// var_dump($e);
+// var_dump($f);
+
+$a = (float)10;//float(10)
+$b = (string)1.3;//string(3) "1.3"
+
+
 var_dump($a);
 var_dump($b);
-var_dump($c);
-var_dump($d);
-var_dump($e);
-var_dump($f);
-
 
 /*
 var_dump()で表示される型
