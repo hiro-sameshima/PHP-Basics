1196fcfd9e85aab4f7a6726f5ddcd136720647f0
鮫島広朗 <hanasaita087@gmail.com>
2021-01-04 09:26:16 +0900

    初期化

--- index.php
+++ index.php
@@ -1,13 +1,2 @@
 <?php
-//ifで条件分岐をする
-//elseifを使って条件を追加する
-
-$score = 70;
-
-if ($score >= 80) {
-  echo "good".PHP_EOL;
-} elseif ($score >= 60) {
-  echo "oh...".PHP_EOL;
-} else {
-  echo "oh my god".PHP_EOL;
-}
+//比較演算子と真偽値
