11e38edf13b7017398b88f0f33ba60fa27ba16e3
鮫島広朗 <hanasaita087@gmail.com>
2021-01-04 22:59:08 +0900

    論理演算子を使わない場合

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 //論理演算子を使ってみる
 /*
+  $scoreが60点で、名前がtaguchiというデータを探す場合。
 */
+
+$score = 60;
+$name = 'tag';
+
+if($score >= 50) {
+  if($name === 'taguchi') {
+    echo 'good job'.PHP_EOL;
+  } else {
+    echo 'bad'.PHP_EOL;
+  }
+} 
