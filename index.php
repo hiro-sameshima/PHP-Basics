82815b932d54ff6643cc60d1293903e53f8b1969
鮫島広朗 <hanasaita087@gmail.com>
2021-01-05 22:59:25 +0900

    初期化

--- index.php
+++ index.php
@@ -1,24 +1,2 @@
 <?php
-//switchを使いこなしている
-//まずは普通のif文での条件分岐
-
-$signal = 'blue';
-
-//switchを使った条件分岐
-//breakを書き忘れると最後まで処理が進んでしまう
-
-switch ($signal) {
-  case 'red';
-    echo 'stop!' .PHP_EOL;
-    break;
-  case 'yellow';
-    echo 'cation!' .PHP_EOL;
-    break;
-  case 'red';
-  case 'yellow';
-    echo 'go!' .PHP_EOL;
-  break;
- default:
-    echo "故障のようだ".PHP_EOL;
-    break;
-}
+//forを使ったループ処理をしよう
