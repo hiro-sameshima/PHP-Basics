6b40e825d3928a671880aea126e0c921cf142bbe
鮫島広朗 <hanasaita087@gmail.com>
2021-01-05 22:43:32 +0900

    switch文での条件分岐

--- index.php
+++ index.php
@@ -11,3 +11,20 @@ if($signal === 'red') {
 } elseif ($signal === 'blue') {
   echo 'go!' .PHP_EOL;
 }
+
+//switchを使った条件分岐
+
+switch ($signal) {
+  case 'red';
+    echo 'stop!' .PHP_EOL;
+    break;
+  case 'yellow';
+    echo 'cation!' .PHP_EOL;
+    break;
+  case 'blue';
+    echo 'go!' .PHP_EOL;
+  break;
+  default:
+    echo "故障のようだ".PHP_EOL;
+    break;
+}
