c51f8e8ee3cd957c32ddf207531bd7c3ca24f349
鮫島広朗 <hanasaita087@gmail.com>
2021-01-05 22:37:25 +0900

    ifを使った条件分岐

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
 //switchで条件分岐をする
+//まずは普通のif文での条件分岐
+
+$signal = 'red';
+
+if($signal === 'red') {
+  echo 'stop!' . PHP_EOL;
+} elseif ($signal === 'yellow') {
+  echo 'cation!' . PHP_EOL;
+} elseif ($signal === 'blue') {
+  echo 'go!' .PHP_EOL;
+}
