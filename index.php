a855937dbc6504ad9581d19e6d67349c3635129f
鮫島広朗 <hanasaita087@gmail.com>
2021-01-05 23:07:31 +0900

    カウンターを表示

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 //forを使ったループ処理をしよう
 
-for ($i = 1; $i <= 5; $i++) {//for(こちらに条件を記述) {　処理  }
-  echo 'hello' .PHP_EOL;
+for ($i = 1; $i <= 5; $i++) {
+  echo "$i  - 'hello'" .PHP_EOL;  //$iの今の値を表示させる際、$iも記述する
 }
+
+/*
+1  - 'hello'
+2  - 'hello'
+3  - 'hello'
+4  - 'hello'
+5  - 'hello'
+と表示される
+*/
