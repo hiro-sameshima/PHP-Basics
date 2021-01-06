e962e16cfe76ceb3660ea7d5357df1d6fc01af63
鮫島広朗 <hanasaita087@gmail.com>
2021-01-06 23:19:46 +0900

    ある倍数だけ処理をスキップする

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 //continue,breakを使った式
 // 特定の処理をスキップする
 
-//continueを使い、4の数字だけスキップする
+//continueを使い、3の倍数だけスキップする
 for ($i = 1; $i <= 10; $i++) {
-  if($i === 4) {
-    continue;
+  if($i % 3 === 0) {
+  continue;
   }
   echo $i .PHP_EOL;
 }
