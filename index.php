257d7ecadd303c383aa490304c2303ffae16be64
鮫島広朗 <hanasaita087@gmail.com>
2021-01-07 23:04:45 +0900

    引数を使って値を関数の呼び出しごとに変えてみる

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 //関数で処理をまとめてみよう
-//広告を関数化する
+//引数を使う
+//関数の値を、変えたい場合、引数として渡してあげる
 
-function showAd() {
+function showAd($message) {
 
   echo '----------------'.PHP_EOL;
-  echo "-------Add------".PHP_EOL;
+  echo '-------'.$message.'------'.PHP_EOL;
   echo '----------------'.PHP_EOL;
 
 }
 
 
-echo showAd();
+echo showAd('あああ');
 echo 'Tom is great!!' .PHP_EOL;
-echo showAd();
+echo showAd('いいい');
 echo 'Bob is great!!' .PHP_EOL;
-echo showAd();
+echo showAd('ううう');
 echo 'Steve is great!!' .PHP_EOL;
-echo showAd();
+echo showAd('えええ');
 echo 'Bob is great!!' .PHP_EOL;
