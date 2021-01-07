703c464262c0e5c444c5f90d527698a3d739f19b
鮫島広朗 <hanasaita087@gmail.com>
2021-01-07 22:54:49 +0900

    関数にまとめた

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
 //関数で処理をまとめてみよう
-//このプログラムに広告を挟むとして。
+//広告を関数化する
+
+function showAd() {
+
+  echo '----------------'.PHP_EOL;
+  echo "-------Add------".PHP_EOL;
+  echo '----------------'.PHP_EOL;
+
+}
 
 
-echo '----------------'.PHP_EOL;
-echo '-------Add------'.PHP_EOL;
-echo '----------------'.PHP_EOL;
+echo showAd();
 echo 'Tom is great!!' .PHP_EOL;
-echo '----------------'.PHP_EOL;
-echo '-------Add------'.PHP_EOL;
-echo '----------------'.PHP_EOL;
+echo showAd();
 echo 'Bob is great!!' .PHP_EOL;
-echo '----------------'.PHP_EOL;
-echo '-------Add------'.PHP_EOL;
-echo '----------------'.PHP_EOL;
+echo showAd();
 echo 'Steve is great!!' .PHP_EOL;
-echo '----------------'.PHP_EOL;
-echo '-------Add------'.PHP_EOL;
-echo '----------------'.PHP_EOL;
+echo showAd();
 echo 'Bob is great!!' .PHP_EOL;
