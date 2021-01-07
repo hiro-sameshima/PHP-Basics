f43acfcdbb053ce9baee3aed6ab3a38bf131d2dd
鮫島広朗 <hanasaita087@gmail.com>
2021-01-07 22:47:45 +0900

    導入②

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 //関数で処理をまとめてみよう
-//このような文章が表示されるプログラム
+//このプログラムに広告を挟むとして。
+
+
+echo '----------------'.PHP_EOL;
+echo '-------Add------'.PHP_EOL;
+echo '----------------'.PHP_EOL;
 echo 'Tom is great!!' .PHP_EOL;
+echo '----------------'.PHP_EOL;
+echo '-------Add------'.PHP_EOL;
+echo '----------------'.PHP_EOL;
 echo 'Bob is great!!' .PHP_EOL;
+echo '----------------'.PHP_EOL;
+echo '-------Add------'.PHP_EOL;
+echo '----------------'.PHP_EOL;
 echo 'Steve is great!!' .PHP_EOL;
+echo '----------------'.PHP_EOL;
+echo '-------Add------'.PHP_EOL;
+echo '----------------'.PHP_EOL;
 echo 'Bob is great!!' .PHP_EOL;
