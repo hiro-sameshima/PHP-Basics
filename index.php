d5f74a75f278f20af8cd95800bafd67773627203
鮫島広朗 <hanasaita087@gmail.com>
2021-01-08 22:50:08 +0900

    初期化

--- index.php
+++ index.php
@@ -1,23 +1,2 @@
 <?php
-//関数で処理をまとめてみよう
-//引数を使う
-//引数を渡さなかった時のデフォルト値を設定できる
-
-function showAd($message = 'Ad') {//〇〇 = 'Ad'とすることでデフォルト値を設定できる
-
-  echo '----------------'.PHP_EOL;
-  echo '-------'.$message.'------'.PHP_EOL;
-  echo '----------------'.PHP_EOL;
-
-}
-
-
-echo showAd('あああ');
-echo 'Tom is great!!' .PHP_EOL;
-echo showAd('いいい');
-echo 'Bob is great!!' .PHP_EOL;
-echo showAd('ううう');
-echo 'Steve is great!!' .PHP_EOL;
-echo showAd();// 引数を渡さなかった
-echo 'Bob is great!!'
- .PHP_EOL;
+//returnを使って処理を返す
