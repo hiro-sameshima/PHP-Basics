1f4ee963e612d8df0eee9f348136a1368bc74d9a
鮫島広朗 <hanasaita087@gmail.com>
2021-01-11 15:03:10 +0900

    名前と点数を渡し、表示する関数

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 //引数の型を指定する
+
+function showInfo($name , $score) {
+
+  echo $name . ':' . $score . PHP_EOL;
+}
+
+// showInfo('tanaka',100);
+
+//誤って関係ない文字列を渡してしまった場合
+showInfo('tanaka' , 'doitnow'); //普通にtanaka:doitnowを表示される
