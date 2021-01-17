1d27663b79d678b33e0af095704db1b0b9c50a6d
鮫島広朗 <hanasaita087@gmail.com>
2021-01-17 18:47:38 +0900

    初期化

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
-//可変長引数
-// function sum($a, $b, $c)
-//引数の数を追加しても計算される
+//
 function sum(...$numbers)
 {
-  // return $a + $b + $c;
   $total = 0;
   foreach ($numbers as $number) {
     $total += $number;
   }
   return $total;
 }
-
-echo sum(1, 3, 5) . PHP_EOL;
-echo sum(4, 2, 5, 1) . PHP_EOL;
