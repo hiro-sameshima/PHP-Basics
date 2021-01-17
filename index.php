e8b4edb28e8596eab363b534c5e03ef6c7443599
鮫島広朗 <hanasaita087@gmail.com>
2021-01-17 18:55:10 +0900

    返り値を配列とし、値の合計と平均を格納する

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
 //
-function sum(...$numbers)
+function getStats(...$numbers)
 {
   $total = 0;
   foreach ($numbers as $number) {
     $total += $number;
   }
-  return $total;
+  return [$total , $total / count($numbers)];//count()とは、配列の要素数を求める
 }
+
+print_r(getStats(1,2,3,4,5));
+
+/*
+Array
+(
+    [0] => 15 値の合計
+    [1] => 3　値の平均
+)
+*/
